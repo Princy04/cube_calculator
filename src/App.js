a5c0afcbbbdd19be5eacb851323f64aac2ccf672
Princy <princymaria004@gmail.com>
2019-10-07 16:40:31 +0530

    Cubic Calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
 import Background from "./assests/images/cubeBackground.png";
-
+import "./App.css";
+const regex = /^\d+(\.\d{1,20})?$/;
 var backgroundImage = {
   backgroundImage: `url(${Background})`
 }
@@ -19,28 +20,54 @@ class CubicCalculator extends React.Component {
     }
   }
 
-  changeHandler = event => {
-    this.setState({ [event.target.name]: event.target.value });
+  changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
+
+  Enter = e => {
+    if (e.key === "Enter") {
+      this.Calculate(e);
+    }
+  }
 
   Calculate = e => {
     e.preventDefault();
-    var lengthInches = this.state.Length_Inches / 12;
-    var widthInches = this.state.Width_Inches / 12;
-    var heightInches = this.state.Height_Inches / 12;
-    this.state.Result = ((parseInt(this.state.Length) + lengthInches) * (parseInt(this.state.Width) + widthInches) * (parseInt(this.state.Height) + heightInches)).toFixed(2);
-    this.setState({ ...this.state.Result })
-  };
+    var isValidNumber =
+      (regex.test(this.state.Length) && regex.test(this.state.Width) && regex.test(this.state.Height))
+        || (regex.test(this.state.Length_Inches) && regex.test(this.state.Width_Inches) && regex.test(this.state.Height_Inches))
+        ? true
+        : false;
+    if (this.state.Length === '') {
+      this.setState({ LengthErrorMessages: "This field is required" });
+    }
+    else {
+      this.setState({ LengthErrorMessages: "" });
+    }
+    if (this.state.Width === '') {
+      this.setState({ WidthErrorMessages: "This field is required" });
+    }
+    else {
+      this.setState({ WidthErrorMessages: "" });
+    }
+    if (this.state.Height === '') {
+      this.setState({ HeightErrorMessages: "This field is required" });
+    }
+    else {
+      this.setState({ HeightErrorMessages: "" });
+    }
+    if (isValidNumber) {
+      var lengthInches = this.state.Length_Inches / 12;
+      var widthInches = this.state.Width_Inches / 12;
+      var heightInches = this.state.Height_Inches / 12;
+      this.state.Result = ((parseFloat(this.state.Length) + lengthInches) * (parseFloat(this.state.Width) + widthInches) * (parseFloat(this.state.Height) + heightInches)).toFixed(2);
+      this.setState({ ...this.state.Result })
+    };
+  }
 
   Reset = () => {
     this.setState({
-      Length: "",
-      Width: "",
-      Height: "",
-      Length_Inches: "",
-      Width_Inches: "",
-      Height_Inches: "",
-      Result: "",
+      Length: "", Width: "", Height: "", Length_Inches: "", Width_Inches: "", Height_Inches: "",
+      Result: "", LengthErrorMessages: "", WidthErrorMessages: "", HeightErrorMessages: ""
     });
   }
 
@@ -51,9 +78,7 @@ class CubicCalculator extends React.Component {
           <MDBCol className="col-md-5 mx-auto">
             <MDBCard className="cardBody" style={backgroundImage}>
               <MDBCardBody>
-                <form className="needs-validation"
-                  onSubmit={this.Calculate}
-                  noValidate>
+                <form>
                   <p className="cyan-text h5 text-center py-3">Cubic Feet Calculator</p>
                   <div className="grey-text">
                     <div>
@@ -61,63 +86,40 @@ class CubicCalculator extends React.Component {
                         <div className="col">
                           <div className="md-form mt-0">
                             <i className="fas fa-ruler prefix"></i>
-                            <input type="number" className="form-control" placeholder="Length"
-                              autoFocus={true} value={this.state.Length} name="Length" pattern="^[0-9]\d*(\.\d+)?$"
-                              required
+                            <input type="text" className="form-control" placeholder="Length"
+                              autoFocus={true} value={this.state.Length} name="Length"
                               onChange={this.changeHandler}
-                              onKeyPress={e => {
-                                if (e.key === "Enter") {
-                                  this.Calculate(e);
-                                }
+                              label="Length"
+                              onBlur={() => {
+                                this.setState({ LengthErrorMessage: "Enter a valid number" });
                               }}
+                              onKeyPress={this.Enter}
                             />
+                            {this.state.Length && !regex.test(this.state.Length) ? (
+                              <div className="errorMessage">
+                                {this.state.LengthErrorMessage}
+                              </div>
+                            ) : (<div className="errorMessage">
+                              {this.state.LengthErrorMessages}
+                            </div>)}
                           </div>
                         </div>
                         <div className="col">
                           <div className="md-form mt-0">
-                            <input type="number" className="form-control" name="Length_Inches" placeholder="Inches"
-                              value={this.state.Length_Inches} pattern="^[0-9]\d*(\.\d+)?$"
-                              required
+                            <input type="text" className="form-control" name="Length_Inches" placeholder="Inches"
+                              value={this.state.Length_Inches}
                               onChange={this.changeHandler}
-                              onKeyPress={e => {
-                                if (e.key === "Enter") {
-                                  this.Calculate(e);
-                                }
+                              label="Length_Inches"
+                              onBlur={() => {
+                                this.setState({ Length_InchesErrorMessage: "Enter a valid number" });
                               }}
+                              onKeyPress={this.Enter}
                             />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div>
-                      <div className="row">
-                        <div className="col">
-                          <div className="md-form mt-0">
-                            <i className="fas fa-ruler prefix"></i>
-                            <input type="number" className="form-control" placeholder="Width"
-                              value={this.state.Width} name="Width" pattern="^[0-9]\d*(\.\d+)?$"
-                              required
-                              onChange={this.changeHandler}
-                              onKeyPress={e => {
-                                if (e.key === "Enter") {
-                                  this.Calculate(e);
-                                }
-                              }}
-                            />
-                          </div>
-                        </div>
-                        <div className="col">
-                          <div className="md-form mt-0">
-                            <input type="number" className="form-control" name="Width_Inches" placeholder="Inches"
-                              value={this.state.Width_Inches} pattern="^[0-9]\d*(\.\d+)?$"
-                              required
-                              onChange={this.changeHandler}
-                              onKeyPress={e => {
-                                if (e.key === "Enter") {
-                                  this.Calculate(e);
-                                }
-                              }}
-                            />
+                            {this.state.Length_Inches && !regex.test(this.state.Length_Inches) ? (
+                              <div className="errorMessage">
+                                {this.state.Length_InchesErrorMessage}
+                              </div>
+                            ) : ("")}
                           </div>
                         </div>
                       </div>
@@ -127,31 +129,82 @@ class CubicCalculator extends React.Component {
                         <div className="col">
                           <div className="md-form mt-0">
                             <i className="fas fa-ruler prefix"></i>
-                            <input type="number" className="form-control" placeholder="Height"
-                              value={this.state.Height} name="Height" pattern="^[0-9]\d*(\.\d+)?$"
-                              required
+                            <input type="text" className="form-control" placeholder="Width"
+                              value={this.state.Width} name="Width"
+                              label="Width"
                               onChange={this.changeHandler}
-                              onKeyPress={e => {
-                                if (e.key === "Enter") {
-                                  this.Calculate(e);
-                                }
+                              onBlur={() => {
+                                this.setState({ WidthErrorMessage: "Enter a valid number" });
                               }}
+                              onKeyPress={this.Enter}
                             />
-
+                            {this.state.Width && !regex.test(this.state.Width) ? (
+                              <div className="errorMessage">
+                                {this.state.WidthErrorMessage}
+                              </div>
+                            ) : (<div className="errorMessage">
+                              {this.state.WidthErrorMessages}
+                            </div>)}
                           </div>
                         </div>
                         <div className="col">
                           <div className="md-form mt-0">
-                            <input type="number" className="form-control" name="Height_Inches" placeholder="Inches"
+                            <input type="text" className="form-control" name="Width_Inches" placeholder="Inches"
+                              value={this.state.Width_Inches}
+                              label="Width_Inches"
+                              onChange={this.changeHandler}
+                              onBlur={() => {
+                                this.setState({ Width_InchesErrorMessage: "Enter a valid number" });
+                              }}
+                              onKeyPress={this.Enter}
+                            />
+                            {this.state.Width_Inches && !regex.test(this.state.Width_Inches) ? (
+                              <div className="errorMessage">
+                                {this.state.Width_InchesErrorMessage}
+                              </div>
+                            ) : ("")}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="row">
+                        <div className="col">
+                          <div className="md-form mt-0">
+                            <i className="fas fa-ruler prefix"></i>
+                            <input type="text" className="form-control" placeholder="Height"
+                              value={this.state.Height} name="Height"
+                              onChange={this.changeHandler}
+                              onBlur={() => {
+                                this.setState({ HeightErrorMessage: "Enter a valid number" });
+                              }}
+                              onKeyPress={this.Enter}
+                            />
+                            {this.state.Height && !regex.test(this.state.Height) ? (
+                              <div className="errorMessage">
+                                {this.state.HeightErrorMessage}
+                              </div>
+                            ) : (<div className="errorMessage">
+                              {this.state.HeightErrorMessages}
+                            </div>)}
+                          </div>
+                        </div>
+                        <div className="col">
+                          <div className="md-form mt-0">
+                            <input type="text" className="form-control" name="Height_Inches" placeholder="Inches"
+                              label="Height_Inches"
                               value={this.state.Height_Inches}
-                              required
                               onChange={this.changeHandler}
-                              onKeyPress={e => {
-                                if (e.key === "Enter") {
-                                  this.Calculate(e);
-                                }
+                              onBlur={() => {
+                                this.setState({ Height_InchesErrorMessage: "Enter a valid number" });
                               }}
+                              onKeyPress={this.Enter}
                             />
+                            {this.state.Height_Inches && !regex.test(this.state.Height_Inches) ? (
+                              <div className="errorMessage">
+                                {this.state.Height_InchesErrorMessage}
+                              </div>
+                            ) : ("")}
                           </div>
                         </div>
                       </div>
@@ -159,17 +212,19 @@ class CubicCalculator extends React.Component {
                     <MDBInput
                       icon="cube"
                       type="text"
-                      name="Result"
-                      value={(isNaN(this.state.Result)) ? (0 + " ft3") : this.state.Result === "" ? '' : this.state.Result + " ft3"}
+                      value={(isNaN(this.state.Result)) ? (0 + "ft3") : this.state.Result === "" ? ("") : this.state.Result + " ft3"}
                       label="Result">
                     </MDBInput>
                   </div>
                   <div className="col">
                     <div className="md-form mt-0">
                       <div className="text-center py-4 mt-3">
-                        <MDBBtn icon="cube" color="cyan" type="submit" onClick={this.Calculate}>
+                        <MDBBtn icon="cube" color="cyan" onClick={this.Calculate}>
                           Calculate
-                </MDBBtn>
+                       </MDBBtn>
+                        <MDBBtn icon="cube" color="cyan" onClick={this.Reset}>
+                          Reset
+                       </MDBBtn>
                       </div>
                     </div>
                   </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CubeCalculator from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<CubeCalculator />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
